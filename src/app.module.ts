ad0a409391f1505d122bc5852678cf5e4cafc04a
ekecardinal <ekecardinal@gmail.com>
2024-09-02 13:34:02 +0100

    added a git ignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,30 @@ import { join } from 'path';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AcccessTokenGuard } from './auth/guards/accessToken.guard';
+import { config } from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
+      useFactory: (config: ConfigService) => {
+        return {
+          cors: {
+            origin: config.get('CLIENT_URL'),
+          },
+          autoSchemaFile: join(
+            process.cwd(),
+            config.get<string>('SCHEMA_PATH'),
+          ),
+          sortSchema: true,
+          playground: true,
+        };
+      },
+      inject: [ConfigService],
     }),
     AuthModule,
     UserModule,
